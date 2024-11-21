b2cb29d63554be79d7c6acbd491d137764a0c3d6
Stephen882-pixel <stephenondeyo0@gmail.com>
2024-11-21 19:04:52 +0300

    slugify category pages url

--- theblog/views.py
+++ theblog/views.py
@@ -47,6 +47,6 @@ class AddCategoryView(CreateView):
 
 
 def CategoryView(request, cats):
-    category_posts = Post.objects.filter(category=cats)
-    return render(request, 'categories.html', {'cats': cats.title(), 'category_posts': category_posts})
+    category_posts = Post.objects.filter(category=cats.replace('-',' '))
+    return render(request, 'categories.html', {'cats': cats.title().replace('-',' '), 'category_posts': category_posts})
 
