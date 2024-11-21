a6e5be2947429ce351f53efc03ba574d993ec915
Stephen882-pixel <stephenondeyo0@gmail.com>
2024-11-21 18:52:39 +0300

    Blog category pages

--- theblog/views.py
+++ theblog/views.py
@@ -12,8 +12,9 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    #ordering = ['-id']
+    # ordering = ['-id']
     ordering = ['-post_date']
+
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -23,8 +24,7 @@ class ArticleDetailView(DetailView):
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'Add_Post.html'
-    # fields = '__all__'
+    template_name = 'Add_Post.html'  # fields = '__all__'
 
 
 class UpdatePostView(UpdateView):
@@ -39,9 +39,14 @@ class DeletePostView(DeleteView):
     success_url = reverse_lazy('home')
 
 
-
 class AddCategoryView(CreateView):
     model = Category
-    #form_class = PostForm
+    # form_class = PostForm
     template_name = 'Add_Category.html'
     fields = '__all__'
+
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats': cats.title(), 'category_posts': category_posts})
+
