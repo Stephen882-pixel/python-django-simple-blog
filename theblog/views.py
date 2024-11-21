afdf715d1e889b555a87783f4004d39f1b151f21
Stephen882-pixel <stephenondeyo0@gmail.com>
2024-11-21 19:52:13 +0300

    Add blog categories in navbar links

--- theblog/views.py
+++ theblog/views.py
@@ -15,10 +15,23 @@ class HomeView(ListView):
     # ordering = ['-id']
     ordering = ['-post_date']
 
+    def get_context_data(self, *args,**kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView,self).get_context_data(*args,**kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
+
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'Articles_Details.html'
+
+    def get_context_data(self, *args,**kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView,self).get_context_data(*args,**kwargs)
+        context['cat_menu'] = cat_menu
+        return context
 
 
 class AddPostView(CreateView):
@@ -50,3 +63,6 @@ def CategoryView(request, cats):
     category_posts = Post.objects.filter(category=cats.replace('-',' '))
     return render(request, 'categories.html', {'cats': cats.title().replace('-',' '), 'category_posts': category_posts})
 
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'categoty_list.html', {'cat_menu_list': cat_menu_list})
